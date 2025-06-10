73b8b2aa8c1c923b244e6bee7c009c8d12d94ac9
RaihaneBenjilali <raihane.benjilali@student.odisee.be>
2025-06-10 10:11:54 +0200

    setup

---
+++ src/main/java/be/ehb/aquafin/AquafinCase2Application.java
@@ -0,0 +1,13 @@
+package be.ehb.aquafin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AquafinCase2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AquafinCase2Application.class, args);
+    }
+
+}
