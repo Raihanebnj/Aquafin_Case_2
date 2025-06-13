59b5f9522ffb596909819d1f9773412c4e4ea206
IgRiZ1 <dalil.blh@outlook.be>
2025-06-13 10:09:51 +0200

    documentatie
    het documentatie voor controllers.

--- src/main/java/be/ehb/aquafin/Controller/indexController.java
+++ src/main/java/be/ehb/aquafin/Controller/indexController.java
@@ -8,38 +8,52 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.util.*;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+/**
+ * Controller voor de hoofdpagina van de Aquafin neerslagapplicatie.
+ * Verzorgt de weergave van neerslaginformatie, voorspellingen en overstromingswaarschuwingen.
+ */
 @Controller
 public class indexController {
 
     @Autowired
     private NeerslagService neerslagService;
 
+    /**
+     * Verwerkt GET-verzoeken voor de hoofdpagina ("/").
+     *
+     * Verzamelt en bereidt de volgende data voor:
+     * - Grafiekdata voor neerslagvisualisatie (5 dagen)
+     * - Tekstuele neerslagvoorspelling
+     * - Overstromingswaarschuwingen (over 3 dagen)
+     *
+     * @param model Spring MVC Model voor het doorgeven van data aan de view
+     * @return String naam van de view template ("index")
+     */
     @GetMapping("/")
     public String index(Model model) {
+        // Haal grafiekdata op voor 5 dagen
+        Map<String, Object> grafiekData = neerslagService.getGrafiekData(5);
 
-
-        //  data voor grafiek én tekst
-        int dagen = 5;
-
-        Map<String, Object> grafiekData = neerslagService.getGrafiekData(dagen);
-
-
+        // Zet grafiekdata om naar JSON voor JavaScript gebruik
         ObjectMapper mapper = new ObjectMapper();
         try {
             String regenGrafiekData = mapper.writeValueAsString(grafiekData);
             model.addAttribute("regenGrafiekData", regenGrafiekData);
         } catch (Exception e) {
+            // Bij fout, stuur leeg JSON object
             model.addAttribute("regenGrafiekData", "{}");
         }
 
+        // Voeg afzonderlijke grafiekcomponenten toe aan model
         model.addAttribute("grafiekDatums", grafiekData.get("datums"));
         model.addAttribute("grafiekGemiddelde", grafiekData.get("gemiddelde"));
         model.addAttribute("grafiekPerStad", grafiekData.get("perStad"));
 
+        // Voeg tekstuele voorspelling toe
         String regenVoorspelling = neerslagService.getRegenVoorspellingTekst();
         model.addAttribute("regenVoorspelling", regenVoorspelling);
 
-        // Overstromingsgevaar
+        // Bereid overstromingswaarschuwingen voor (3 dagen vooruit)
         Map<String, Object> waarschuwing = neerslagService.getOverstromingsWaarschuwing(3);
         model.addAttribute("overstromingsgevaar", waarschuwing.get("gevaar"));
         model.addAttribute("risicosteden", waarschuwing.get("risicosteden"));
